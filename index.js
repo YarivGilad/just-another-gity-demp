9e50c81c61c416fd76e0ab74997c04e144cd03a3
--global <yariv.gilad@gmail.com>
2019-08-15 17:40:04 +0300

    added some colorful logs

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const log = require('@ajar/marker')
 
 log.info('hello marker')
+
+log.blue('blue','ocean');       
+log.magenta('magenta','is lighter then purple');    
+log.cyan('cyan','is lighter then blue');       
+log.green('green','peace');      
+log.yellow('yellow','mellow');     
+log.red('red','tape');  
