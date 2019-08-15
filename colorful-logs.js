bcb1615801be8c511e5c69d083181dc27710baaf
--global <yariv.gilad@gmail.com>
2019-08-15 17:49:54 +0300

    externelized the colored logs

---
+++ colorful-logs.js
@@ -0,0 +1,11 @@
+const log = require('@ajar/marker');
+
+const log_in_colors = ()=> {
+    log.blue('blue','ocean');       
+    log.magenta('magenta','is lighter then purple');    
+    log.cyan('cyan','is lighter then blue');       
+    log.green('green','peace');      
+    log.yellow('yellow','mellow');     
+    log.red('red','tape');  
+}
+module.exports = {log_in_colors};
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const log = require('@ajar/marker')
 
+const {log_in_colors} = require('./colorful-logs')
+
 log.info('hello marker')
 
-log.blue('blue','ocean');       
-log.magenta('magenta','is lighter then purple');    
-log.cyan('cyan','is lighter then blue');       
-log.green('green','peace');      
-log.yellow('yellow','mellow');     
-log.red('red','tape');  
+log_in_colors();
