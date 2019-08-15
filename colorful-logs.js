8129845ad4a60a81792d7e965365cf7d36f1391f
--global <yariv.gilad@gmail.com>
2019-08-15 17:51:07 +0300

    changed order of colors

--- colorful-logs.js
+++ colorful-logs.js
@@ -2,10 +2,10 @@ const log = require('@ajar/marker');
 
 const log_in_colors = ()=> {
     log.blue('blue','ocean');       
-    log.magenta('magenta','is lighter then purple');    
+    log.red('red','tape');  
     log.cyan('cyan','is lighter then blue');       
     log.green('green','peace');      
+    log.magenta('magenta','is lighter then purple');    
     log.yellow('yellow','mellow');     
-    log.red('red','tape');  
 }
 module.exports = {log_in_colors};
